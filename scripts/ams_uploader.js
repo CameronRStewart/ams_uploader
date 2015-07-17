e48efc81d2a4b6a418c922f368c60ad6acb1adbd
Cameron Stewart <bacchus@unm.edu>
2015-07-17 15:57:33 -0600

    added javascript to uncheck/disable authenticated box when entering anything via media url.

---
+++ scripts/ams_uploader.js
@@ -0,0 +1,17 @@
+(function ($) {
+
+    Drupal.behaviors.ams_uploader = {
+        attach: function (context) {
+            $('input#edit-field-media-url-und-0-value').change(function() {
+                if( $('input#edit-field-media-url-und-0-value').val().length > 0 ){
+                    $('#edit-field-authenticated-und').attr('checked', false);
+                    $('#edit-field-authenticated-und').attr('disabled', true);
+                }
+                else {
+                    $('#edit-field-authenticated-und').removeAttr("disabled");
+                }
+            });
+        }
+    }
+
+})(jQuery);
