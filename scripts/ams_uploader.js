07abe30e49b14d646e9c88aebc5fcb6e07124ba5
Cameron Stewart <bacchus@unm.edu>
2015-07-17 17:03:49 -0600

    now check if text entered to media url is an http addresss, if so, this greys out auth box.

--- scripts/ams_uploader.js
+++ scripts/ams_uploader.js
@@ -3,7 +3,7 @@
     Drupal.behaviors.ams_uploader = {
         attach: function (context) {
             $('input#edit-field-media-url-und-0-value').change(function() {
-                if( $('input#edit-field-media-url-und-0-value').val().length > 0 ){
+                if( $('input#edit-field-media-url-und-0-value').val().length > 0 && $('input#edit-field-media-url-und-0-value').val().indexOf('http') == 0){
                     $('#edit-field-authenticated-und').attr('checked', false);
                     $('#edit-field-authenticated-und').attr('disabled', true);
                 }
