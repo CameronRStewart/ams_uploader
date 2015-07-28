6dc97c8cf2d640490bc2137ef01f32adad183fad
Cameron Stewart <bacchus@unm.edu>
2015-07-28 11:34:55 -0600

    url input disable dupon upload.

--- scripts/ams_uploader.js
+++ scripts/ams_uploader.js
@@ -2,6 +2,8 @@
 
     Drupal.behaviors.ams_uploader = {
         attach: function (context) {
+
+            /* No Longer Neccesary
             $('input#edit-field-media-url-und-0-value').change(function() {
                 if( $('input#edit-field-media-url-und-0-value').val().length > 0 && $('input#edit-field-media-url-und-0-value').val().indexOf('http') === 0){
                     $('#edit-field-authenticated-und').attr('checked', false);
@@ -11,7 +13,22 @@
                     $('#edit-field-authenticated-und').removeAttr("disabled");
                 }
             });
+            */
+
+
+            setInterval(function () { track_fid_change()}, 1000);
+
         }
     }
+
+    function track_fid_change() {
+        if($('input[name="field_media_upload[und][0][fid]"]').val() == 0) {
+            $("input#edit-field-media-url-und-0-value").removeAttr('disabled');
+        }
+        else {
+            $("input#edit-field-media-url-und-0-value").attr('disabled', true);
+        }
+    };
+
 
 })(jQuery);
