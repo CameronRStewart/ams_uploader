dac678b8d415e01e7c5e811a8e6b9ecf846c87e8
Cameron Stewart <bacchus@unm.edu>
2015-07-28 17:10:00 -0600

    added some comments and took out some TODOs.

--- scripts/ams_uploader.js
+++ scripts/ams_uploader.js
@@ -3,6 +3,7 @@
     Drupal.behaviors.ams_uploader = {
         attach: function (context) {
 
+            // Upon new node creation, if a user enters a URL, disable subdirectory field.
             $('input#edit-field-media-url-und-0-value').change(function() {
                 if( $('input#edit-field-media-url-und-0-value').val().length > 0 && $('input[name="field_media_upload[und][0][fid]"]').val() == 0){
                     $('input#edit-field-directory-und-0-value').attr('disabled', true);
@@ -13,7 +14,7 @@
             });
 
 
-
+            // If a file has been uploaded, disable url input field.
             setInterval(function () { track_fid_change()}, 1000);
 
         }
