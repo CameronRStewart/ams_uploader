49d466b0a9be230f628b5898c38a1c1488817cbc
Cameron Stewart <bacchus@unm.edu>
2015-07-28 16:52:40 -0600

    Disable subdirectory input if creating node with url.

--- scripts/ams_uploader.js
+++ scripts/ams_uploader.js
@@ -3,17 +3,15 @@
     Drupal.behaviors.ams_uploader = {
         attach: function (context) {
 
-            /* No Longer Neccesary
             $('input#edit-field-media-url-und-0-value').change(function() {
-                if( $('input#edit-field-media-url-und-0-value').val().length > 0 && $('input#edit-field-media-url-und-0-value').val().indexOf('http') === 0){
-                    $('#edit-field-authenticated-und').attr('checked', false);
-                    $('#edit-field-authenticated-und').attr('disabled', true);
+                if( $('input#edit-field-media-url-und-0-value').val().length > 0 && $('input[name="field_media_upload[und][0][fid]"]').val() == 0){
+                    $('input#edit-field-directory-und-0-value').attr('disabled', true);
                 }
                 else {
-                    $('#edit-field-authenticated-und').removeAttr("disabled");
+                    $('input#edit-field-directory-und-0-value').removeAttr("disabled");
                 }
             });
-            */
+
 
 
             setInterval(function () { track_fid_change()}, 1000);
